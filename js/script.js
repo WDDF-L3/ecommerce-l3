c02b064320588caea29d7e72c75ec4b165b05906
WDDF-L3 <mahmudul.hasan431603@gmail.com>
2026-04-18 12:36:39 +0600

    fourth commit

--- js/script.js
+++ js/script.js
@@ -7,3 +7,75 @@
       }
       updateDateTime();
       setInterval(updateDateTime, 1000);
+
+//index file data save 
+document.addEventListener("click", function(e){
+
+    if(e.target.classList.contains("add-to-cart")){
+
+        let product = {
+            name: e.target.dataset.name,
+            price: e.target.dataset.price,
+            img: e.target.dataset.img,
+            qty: 1
+        };
+
+        let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+        cart.push(product);
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+
+        alert("Added to cart!");
+    }
+
+});
+
+// shopping cart e add 
+document.addEventListener("DOMContentLoaded", function(){
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let container = document.getElementById("cart-items");
+
+    cart.forEach(item => {
+
+        let html = `
+        <div class="row align-items-center mb-3 cart-item">
+            <div class="col-md-3">
+                <img src="${item.img}" class="img-fluid">
+            </div>
+            <div class="col-md-4">
+                <h6>${item.name}</h6>
+                <small>BDT - ${item.price}</small>
+            </div>
+            <div class="col-md-2">
+                <input type="number" class="form-control quantity" value="${item.qty}" min="1" data-price="${item.price}">
+            </div>
+            <div class="col-md-2">
+                <strong class="item-total">৳${item.price * item.qty}</strong>
+            </div>
+            <div class="col-md-1">
+                <button class="btn btn-danger btn-sm remove-item">×</button>
+            </div>
+        </div>
+        `;
+
+        container.insertAdjacentHTML("beforeend", html);
+    });
+
+});
+
+// product remove and update 
+document.addEventListener("click", function(e){
+    if(e.target.classList.contains("remove-item")){
+
+        let index = [...document.querySelectorAll(".remove-item")].indexOf(e.target);
+
+        let cart = JSON.parse(localStorage.getItem("cart")) || [];
+        cart.splice(index, 1);
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+
+        e.target.closest(".cart-item").remove();
+    }
+});
