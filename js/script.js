eededa9f53d705884dfc622a4f598d5454ddb71f
Web & Dev <mahmudul.hasan431603@gmail.com>
2026-04-22 09:12:40 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -120,3 +120,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+
+// function updateDateTime() {
+//       const now = new Date();
+//       const options = {
+//         weekday: 'long',
+//         year: 'numeric',
+//         month: 'long',
+//         day: 'numeric',
+//         hour: '2-digit',
+//         minute: '2-digit',
+//         second: '2-digit'
+//       };
+//       const formatted = now.toLocaleString('en-US', options);
+//       document.getElementById("dateTime").textContent = formatted;
+//     }
+//     updateDateTime(); // first run
+//     setInterval(updateDateTime, 1000);
